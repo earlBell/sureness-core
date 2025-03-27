c718e251b97197f1c9cefa7876d12cce5cb1f656
Earl <873440996@qq.com>
2025-03-27 23:17:30 +0800

    feat(初始化): maven相关组件升级完毕，单元测试执行正常

---
+++ src/main/java/com/usthe/sureness/handler/AttachSessionServletHandler.java
@@ -0,0 +1,34 @@
+package com.usthe.sureness.handler;
+
+import com.usthe.sureness.subject.SubjectSum;
+import com.usthe.sureness.util.SurenessConstant;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import static java.util.Objects.isNull;
+
+/**
+ * attach auth success subject info with servlet request session
+ * @author tomsun28
+ * @date 2021/4/5 17:54
+ */
+public class AttachSessionServletHandler implements SuccessHandler{
+
+    @Override
+    public void processHandler(SubjectSum subjectSum, Object request) {
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest servletRequest = (HttpServletRequest) request;
+            HttpSession httpSession = servletRequest.getSession();
+            if (isHttpSessionValid(httpSession)) {
+                httpSession.setAttribute(SurenessConstant.PRINCIPAL, subjectSum.getPrincipal());
+                httpSession.setAttribute(SurenessConstant.PRINCIPALS, subjectSum.getPrincipalMap());
+                httpSession.setAttribute(SurenessConstant.ROLES, subjectSum.getRoles());
+            }
+        }
+
+    }
+
+    private boolean isHttpSessionValid(HttpSession httpSession) {
+        return httpSession.isNew() || isNull(httpSession.getAttribute(SurenessConstant.PRINCIPAL));
+    }
+}
---
+++ src/main/java/com/usthe/sureness/subject/creater/NoneSubjectJaxRsCreator.java
@@ -0,0 +1,31 @@
+package com.usthe.sureness.subject.creater;
+
+import com.usthe.sureness.subject.Subject;
+import com.usthe.sureness.subject.SubjectCreate;
+import com.usthe.sureness.subject.support.NoneSubject;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+
+/**
+ * the subject creator support creating NoneSubject
+ * all request content can create a NoneSubject
+ * only support JAX-RS
+ * @author tomsun28
+ * @date 15:55 2020-02-28
+ */
+public class NoneSubjectJaxRsCreator implements SubjectCreate {
+    @Override
+    public boolean canSupportSubject(Object context) {
+        return context instanceof ContainerRequestContext;
+    }
+
+    @Override
+    public Subject createSubject(Object context) {
+        String requestUri = ((ContainerRequestContext) context).getUriInfo().getPath();
+        String requestType = ((ContainerRequestContext) context).getMethod();
+        String targetUri = requestUri.concat("===").concat(requestType).toLowerCase();
+        return NoneSubject.builder()
+                .setTargetUri(targetUri)
+                .build();
+    }
+}
